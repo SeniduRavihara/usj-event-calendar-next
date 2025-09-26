09f9d60800e2dab7dd98fb27fa324b04c27f3341
Poorna Sachinthana <poornasachinthana2241@gmail.com>
2025-09-26 12:25:43 +0530

    Import Sections

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { 
+  Calendar, 
+  Bell, 
+  User, 
+  Users, 
+  GraduationCap,
+  Search,
+  Filter,
+  Plus,
+  Edit,
+  Trash2,
+  Eye,
+  MapPin,
+  X
+} from 'lucide-react';
